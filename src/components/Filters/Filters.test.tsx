c9a6fe858f5ca60458bf059e44b718693682e402
Yury <yury.nekhaevskiy@gmail.com>
2021-02-24 17:14:25 +0300

    test: add tests to choose specific color and manufacturer in Filters

--- src/components/Filters/Filters.test.tsx
+++ src/components/Filters/Filters.test.tsx
@@ -1,6 +1,7 @@
-import { render, within } from "@testing-library/react";
+import { render, waitFor, within } from "@testing-library/react";
 import React from "react";
 import { Filters } from ".";
+import userEvent from "@testing-library/user-event";
 
 test("colors select is disabled if no colors passed to Filters component", () => {
   const { getByTestId, getByText, getAllByText } = render(<Filters />);
@@ -22,8 +23,40 @@ test("manufacturers select is disabled if no manufacturers passed to Filters com
   expect(getByRole("button")).toHaveAttribute("aria-disabled", "true");
 });
 
-test.todo("specific color can be selected");
+test("specific color can be selected", async () => {
+  const testColors = ["blue", "black", "white"];
+  const { getByText, queryByText } = render(<Filters colors={testColors} />);
 
-test.todo("specific manufacturer can be selected");
+  expect(queryByText(/black/i)).not.toBeInTheDocument();
+  expect(queryByText(/white/i)).not.toBeInTheDocument();
+
+  userEvent.click(getByText(/all car colors/i));
+  userEvent.click(getByText(/black/i));
+
+  await waitFor(() =>
+    expect(queryByText(/all car colors/i)).not.toBeInTheDocument()
+  );
+  expect(getByText(/black/i)).toBeVisible();
+  expect(queryByText(/white/i)).not.toBeInTheDocument();
+});
+
+test("specific manufacturer can be selected", async () => {
+  const testManufacturers = ["bmw", "mercedes", "chrysler"];
+  const { getByText, queryByText } = render(
+    <Filters manufacturers={testManufacturers} />
+  );
+
+  expect(queryByText(/bmw/i)).not.toBeInTheDocument();
+  expect(queryByText(/mercedes/i)).not.toBeInTheDocument();
+
+  userEvent.click(getByText(/all manufacturers/i));
+  userEvent.click(getByText(/bmw/i));
+
+  await waitFor(() =>
+    expect(queryByText(/all manufacturers/i)).not.toBeInTheDocument()
+  );
+  expect(getByText(/bmw/i)).toBeVisible();
+  expect(queryByText(/mercedes/i)).not.toBeInTheDocument();
+});
 
 test.todo("nothing happens if filters are disabled and button is clicked");
